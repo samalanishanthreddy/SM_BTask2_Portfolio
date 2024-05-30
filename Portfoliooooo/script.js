d15b9ae563b9f2c0d5a21f3802a847fa366249a7
samalanishanthreddy <144210944+samalanishanthreddy@users.noreply.github.com>
2024-05-30 18:14:14 +0530

    Add files via upload

---
+++ Portfoliooooo/script.js
@@ -0,0 +1,13 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const links = document.querySelectorAll('a[href^="#"]');
+    for (const link of links) {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    }
+});
